68c886647e2b9103d5452ad9f87555bf80819235
PatrickMcKiernan <Patrick.McKiernan@Zinkwoks.com>
2022-10-04 15:33:52 +0100

    Personal Project Start

---
+++ Personal-Project-URL-Shortener/src/main/java/com/Zinkworks/PersonalProjectURLShortener/PersonalProjectUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.Zinkworks.PersonalProjectURLShortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectUrlShortenerApplication.class, args);
+	}
+
+}
